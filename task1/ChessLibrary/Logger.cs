0998bf52a9efa20d3d5d803681d44630ec4bb711
Ирина Ходанович <hodanovich_ira@mail.ru>
2021-06-09 18:53:26 +0300

    .

---
+++ task1/ChessLibrary/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ChessLibrary
+{
+    public class Logger
+    {
+        private static string path = @"../../LoggerWrite.txt";
+        public static void Write(string message)
+        {
+           
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine(DateTime.Now.ToString() + " " + message);
+            }
+        }
+    }
+}
